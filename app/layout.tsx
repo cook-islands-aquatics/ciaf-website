da04c7ebb9b06fe168d20a6d2fb9eb8826b11c1a
WesleyRoberts <tikiariki@gmail.com>
2026-05-15 17:35:51 +1000

    change hard coded email address

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,7 +63,7 @@ export default async function RootLayout({
     url: 'https://cookislandsaquatics.org',
     logo: 'https://cookislandsaquatics.org/ciaf-logo.png',
     sport: ['Swimming', 'Open Water Swimming'],
-    email: settings.email ?? 'info@ciaf.ck',
+    email: settings.email ?? 'info@cookislandsaquatics.org',
     memberOf: {
       '@type': 'SportsOrganization',
       name: 'World Aquatics',
